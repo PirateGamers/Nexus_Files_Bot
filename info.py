c5afd0838ce2fea09185377ebf6e174649b250a8
PirateGamers <109447641+PirateGamers@users.noreply.github.com>
2025-10-10 11:23:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,8 +99,8 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie7xchat')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/nova_flix')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MoviesFactory72')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/TechnicalBichu')
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/pirategamers')
 MOVIE_UPDATE_CHANNEL_LNK = environ.get('MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/TechnicalBichu')
 OWNERID = int(os.environ.get('OWNERID', '5097048997'))  # Replace with the actual admin ID
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     APP_NAME = environ.get('APP_NAME')
 else:
     ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
+BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'filter-2723.koyeb.app'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
 URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else "https://{}/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
